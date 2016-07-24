e386337f4034b4a5fe218b3a2fd161f0a9ff3c26
Flynn Joffray <nucleardreamer@gmail.com>
2016-07-24 17:23:58 -0400

    first

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,77 @@
+import XMonad
+import System.Exit
+import XMonad.Actions.SpawnOn
+import XMonad.Actions.CycleWS
+import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.OneBig
+import XMonad.Layout.NoBorders
+import XMonad.Layout.LayoutHints
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Prompt.Window
+import XMonad.Prompt.XMonad
+import XMonad.Util.Cursor
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+
+import Control.Applicative
+import Control.Monad
+import Data.Char (toLower)
+import Data.List (isPrefixOf, filter)
+import Data.Maybe
+import Data.Monoid
+import Data.Ratio
+
+import qualified Codec.Binary.UTF8.String as UTF8
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+myModMask = mod1Mask
+
+myLayoutHook = commonManagers $
+           tiled |||
+           Full
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 4/100
+     commonManagers = smartBorders . avoidStruts
+
+myManageHook = composeAll
+    [ manageHook gnomeConfig
+    , isFullscreen --> doFullFloat -- make full-screen windows work
+    , className =? "Xfce4-notifyd" --> doIgnore
+    ]
+
+myStartupHook = do
+  setDefaultCursor xC_left_ptr
+  spawn "xrdb $HOME/.Xresources"
+  spawn "xrandr -s 1920x1080"
+  spawn "xscreensaver -nosplash"
+  spawn "xmobar .xmonad/mobar.conf"
+  return ()
+
+myConfig = ewmh defaultConfig
+       { terminal           = "urxvt"
+       , focusFollowsMouse  = True
+       , modMask            = myModMask
+       , normalBorderColor  = "dim gray"
+       , focusedBorderColor = "firebrick"
+       , layoutHook         = myLayoutHook
+       , manageHook         = myManageHook <+> manageDocks <+> manageSpawn
+       , startupHook        = myStartupHook
+       , logHook            = dynamicLogString xmobarPP >>= xmonadPropLog
+       , handleEventHook    = fullscreenEventHook
+       , clickJustFocuses   = False
+       }
+       `additionalKeys`  -- see /usr/include/X11/keysymdef.h
+       [
+         ((myModMask, xK_grave), toggleWS)
+       ]
+
+main = xmonad myConfig
---
+++ .zshrc
@@ -0,0 +1,61 @@
+#!/bin/zsh
+
+export EDITOR="nano"
+export BROWSER="google-chrome"
+
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export MANPATH="/usr/local/man:$MANPATH"
+export LANG=en_US.UTF-8
+export ARCHFLAGS="-arch x86_64"
+
+setopt always_to_end list_types  # completion
+
+setopt extendedglob hist_expire_dups_first hist_ignore_all_dups hist_ignore_space hist_verify share_history  # history
+HISTSIZE=2000
+SAVEHIST=10000
+HISTFILE=~/.zsh_history
+
+setopt check_jobs interactive_comments rcquotes transient_rprompt
+
+autoload -Uz compinit
+compinit
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+# zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+# zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+autoload -U colors && colors
+autoload -Uz vcs_info
+setopt prompt_subst
+
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+if [ $SSH_CLIENT ]; then
+    DOMAIN='%F{red}%m'
+else
+    DOMAIN='%m'
+fi
+PROMPT='%B%(!.%F{red}.%F{green})%n@$DOMAIN%F{white}%b:%B%F{blue}%~%f${vcs_info_msg_0_}%f %F{cyan}${VIRTUAL_ENV:t}%b%f
+> '
+RPROMPT=''
+
+
+
+[[ -e ~/.zsh_aliases ]] && source ~/.zsh_aliases
